9237a952c66f3e420497d7fb4691f9522ca2c45a
doaa mostafa <50427984+doaamostafa67988@users.noreply.github.com>
2024-05-26 08:40:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def get_response():
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='800', port=5000)
